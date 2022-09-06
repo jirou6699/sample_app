03faa4611a0b706b7d3a9f31bcdb4648152e3162
github jirou6699 <shundom09@gmail.com>
2022-09-07 01:41:09 +0900

    パスワードのバリデーション

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,7 +39,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
 	def setup
-		@user = User.new(name: 'suzuki', email: "aaa@gmail.com")
+		@user = User.new(name: 'kanekiyo', email: "kanekiyo@gmail.com", password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "email addresses should be unique" do
@@ -54,5 +54,15 @@ class UserTest < ActiveSupport::TestCase
 		@user.email = mixed_case_email
 		@user.save
 		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
+	test "password should be present (nonblank)" do
+		@user.password = @user.password_confirmation = "" * 6
+		assert_not @user.valid?
+	end
+
+	test "password should have a minimum length " do
+		@user.password = @user.password_confirmation = "a" * 5
+		assert_not @user.valid?
 	end
 end
