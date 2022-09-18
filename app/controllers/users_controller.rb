a546a0ed1e2c462ead1335e8a3284ed949059e9d
github jirou6699 <shundom09@gmail.com>
2022-09-18 13:29:07 +0900

    destroyアクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:index, :edit, :update]
-	before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+	before_action :admin_user, only: :destroy
 
-	def index
-		@users = User.paginate(page: nil)
-
-	end
+  def index
+    @users = User.paginate(page: nil)
+  end
 
   def show
     @user = User.find(params[:id])
@@ -18,27 +18,32 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-			log_in @user
-			flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-    elsif
-      render "new"
+    elsif render "new"
     end
   end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update(user_params)
-			flash[:success] = "Profile updated"
-			redirect_to @user
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   private
 
@@ -46,16 +51,20 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-	def logged_in_user
-		unless logged_in?                      # ユーザーが存在しない時
-			store_location
-			flash[:danger] = "Please log in."    # フラッシュメッセージを代入
-			redirect_to login_url                # リダイレクトしてログイン画面へ
-		end
-	end
+  def logged_in_user
+    unless logged_in? # ユーザーが存在しない時
+      store_location
+      flash[:danger] = "Please log in."    # フラッシュメッセージを代入
+      redirect_to login_url                # リダイレクトしてログイン画面へ
+    end
+  end
 
-	def correct_user
-		@user = User.find(params[:id])
-		redirect_to(root_url) unless current_user?(@user)
-	end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
