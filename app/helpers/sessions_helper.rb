b9c401d6280ed66151acd14b3ce172ba93e9910f
github jirou6699 <shundom09@gmail.com>
2022-09-21 01:00:46 +0900

    アカウントの有効化の追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,11 +17,11 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)   # 左辺がなければ右辺を代入する
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
-    end
+    end  
   end
 
 	  # 渡されたユーザーがカレントユーザーであればtrueを返す
